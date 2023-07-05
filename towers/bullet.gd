c9726d8b9290e7987832d23958114f6c0f2cbcd7
Matt Maufe <matthewmaufe@hotmail.com>
2023-07-05 23:50:18 +0100

    Gave enemies health bars, damage numbers, and status effect icons. All towers now working. Added explosive bullets.

--- towers/bullet.gd
+++ towers/bullet.gd
@@ -7,22 +7,22 @@ var trajectory
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
-	self.velocity = self.trajectory * SPEED 
-	
-	move_and_slide()
+    self.velocity = self.trajectory * SPEED 
+    
+    move_and_slide()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is Enemy:
-		body.hit(self)
-		
-		die()
+    if body is Enemy:
+        body.hit(self.damage)
+        
+        die()
 
 func die():
-	queue_free()
+    queue_free()
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	die()
+    die()
--- towers/caltrop.gd
+++ towers/caltrop.gd
@@ -1,7 +1,7 @@
 class_name Caltrop extends RigidBody2D
 
-var lifespan 
-var damage 
+var lifespan = 3
+var damage = 1 
 
 const COOLDOWN = 0.1 # 1
 const SPEED = 50
@@ -9,38 +9,35 @@ const SPEED = 50
 var timer
 
 func _ready() -> void:
-	timer = Timer.new()    
-	
-	timer.one_shot = true
-	timer.autostart = false 
-	timer.wait_time = COOLDOWN
-	timer.stop()
-	
-	add_child(timer)
+    timer = Timer.new()    
+    
+    timer.one_shot = true
+    timer.autostart = false 
+    timer.wait_time = COOLDOWN
+    timer.stop()
+    
+    add_child(timer)
 
 # func _physics_process(delta: float) -> void:
-	# pass
-	# self.velocity = self.trajectory * SPEED * delta
-	
-	# self.velocity.y += ProjectSettings.get('physics/2d/default_gravity') * delta
-	
-	# move_and_collide(self.velocity)
-	
-	
+    # pass
+    # self.velocity = self.trajectory * SPEED * delta
+    
+    # self.velocity.y += ProjectSettings.get('physics/2d/default_gravity') * delta
+    
+    # move_and_collide(self.velocity)
+    
 func decrement_lifespan() -> void:
-	self.lifespan -= 1 
-	
-	if self.lifespan <= 0:
-		self.die()
-		
+    self.lifespan -= 1 
+    
+    if self.lifespan <= 0:
+        self.die()
+        
 func die() -> void:
-	queue_free()
+    queue_free()
 
-func _on_detector_body_entered(body: Node2D) -> void:
-	print(body)
-	if body is Enemy: # and timer.is_stopped():
-		print('Stab stab! ' + str(body))
-		body.hit(self.damage)
-		timer.start()
-		
-		decrement_lifespan()
+func _on_area_2d_body_entered(body: Node2D) -> void:
+    if body is Enemy and timer.is_stopped():
+        body.hit(self.damage)
+        timer.start()
+        
+        decrement_lifespan()
