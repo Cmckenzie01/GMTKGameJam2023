94734b8a25d9012f35662f02c365425f67c878cd
Matt Maufe <matthewmaufe@hotmail.com>
2023-07-06 22:14:07 +0100

    Upgraded to Godot 4.1

--- level/UpgradeShop.gd
+++ level/UpgradeShop.gd
@@ -8,40 +8,40 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-	
+    pass # Replace with function body.
+    
 func _process(_delta):
-	if get_tree().paused == true:
-		if Input.is_action_just_pressed("interact"):
-			if paused:
-				interact()
-				paused = false
-			else:
-				paused = true
-	
+    if get_tree().paused == true:
+        if Input.is_action_just_pressed("interact"):
+            if paused:
+                interact()
+                paused = false
+            else:
+                paused = true
+    
 func _on_body_entered(body):
-	if body.name == "Player":
-		body.interaction = self
+    if body.name == "Player":
+        body.interaction = self
 
 func _on_body_exited(body):
-	if body.name == "Player":
-		body.interaction = null
-	
-	if shopActive:
-		shopActive = false
-		get_tree().get_root().get_node("LevelOne/GUI/ShopGUI").visible = false
-		txt = "ENTER SHOP"
+    if body.name == "Player":
+        body.interaction = null
+    
+    if shopActive:
+        shopActive = false
+        get_tree().get_root().get_node("LevelOne/GUI/ShopGUI").visible = false
+        txt = "ENTER SHOP"
 
 func interact():
-	if shopActive == false:
-		shopActive = true
-		get_tree().get_root().get_node("LevelOne/GUI/ShopGUI").visible = true
-		txt = "LEAVE SHOP"
-		playerBody.interaction = self
-		get_tree().paused = true
-	elif shopActive == true:
-		shopActive = false
-		get_tree().get_root().get_node("LevelOne/GUI/ShopGUI").visible = false
-		txt = "ENTER SHOP"
-		playerBody.interaction = self
-		get_tree().paused = false
+    if shopActive == false:
+        shopActive = true
+        get_tree().get_root().get_node("LevelOne/GUI/ShopGUI").visible = true
+        txt = "LEAVE SHOP"
+        playerBody.interaction = self
+        get_tree().paused = true
+    elif shopActive == true:
+        shopActive = false
+        get_tree().get_root().get_node("LevelOne/GUI/ShopGUI").visible = false
+        txt = "ENTER SHOP"
+        playerBody.interaction = self
+        get_tree().paused = false
