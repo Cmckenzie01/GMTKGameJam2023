699b32cd0b8267b7007e7a24990cf097379ccea4
Matt Maufe <matthewmaufe@hotmail.com>
2023-07-08 01:10:11 +0100

    Added some basic stuff. Cards that expand on hover and a room node.

---
+++ scenes/Floors/Room.gd
@@ -0,0 +1,14 @@
+class_name Room extends Node2D
+
+@export var room_name: String = 'Test Room'
+
+@onready var room_name_node = $RoomName
+@onready var icon_node = $Icon
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    self.room_name_node.text = self.room_name
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    pass
