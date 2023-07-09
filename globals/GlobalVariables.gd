f7a696049e48f16293f5dc51ca617ceaff591f28
Matt Maufe <matthewmaufe@hotmail.com>
2023-07-09 13:04:06 +0100

    Added different stats and bonuses and rewards for rooms. Updated the dark majesty's title

--- globals/GlobalVariables.gd
+++ globals/GlobalVariables.gd
@@ -12,6 +12,8 @@ func read_json_file(file_path):
 
 @onready var DungeonFloors: Array = [preload("res://levels/three_room_dungeon.tscn")]#[preload("res://levels/three_room_dungeon.tscn"), preload("res://levels/four_room_dungeon.tscn"), preload("res://levels/five_room_dungeon.tscn")]
 
+@export var dark_lord_title = 'Your Shadowy Majesty'
+
 enum CardType {
 	TILE,
 	BUFF
